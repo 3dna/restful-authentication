d9437a9a123e8438bc29f183b08e2d099b40daba
Aaron Qian <aq1018@gmail.com>
2012-11-15 18:32:13 -0800

    load modules when gem is required

---
+++ lib/restful-authentication.rb
@@ -0,0 +1,3 @@
+require File.join(File.dirname(__FILE__), "authentication")
+require File.join(File.dirname(__FILE__), "authentication", "by_password")
+require File.join(File.dirname(__FILE__), "authentication", "by_cookie_token")
